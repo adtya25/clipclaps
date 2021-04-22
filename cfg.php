4eaa7828b04f32c011b5273fedd07b8a546b1bc0
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-04-22 10:43:05 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// data [ api.cc.lerjin.com ] \\
+
+/*-------------------------------*/
+$external_version = "2.6.1.4";
+$device = "10";
+$timezone = "7";
+$version = "42";
+/*-------------------------------*/
+$token = "NmNlNzVlODQtMmEwZS00YWE3LTliOWMtN2IyYmI3MjBiY2Qy";
+$uuid = "b0f025780990c47c";
+$lang = "in";
+$userid = "47905981";
+/*-------------------------------*/
+
+?>
